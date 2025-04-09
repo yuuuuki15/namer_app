05bcd85a8c16ec3f46a8ddd97d0545d1cb0d8d4b
yuuuuki15 <rx86340616@gmal.com>
2025-04-09 12:55:53 +0200

    make it pretty

--- lib/main.dart
+++ lib/main.dart
@@ -38,19 +38,56 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
+    var pair = appState.current;
 
     return Scaffold(
-      body: Column(
-        children: [
-          Text('A random AWESOME idea:'),
-          Text(appState.current.asLowerCase),
-          ElevatedButton(
-            onPressed: () => {
-              appState.getNext()
-            },
-            child: Text('next'),
-          ),
-        ],
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BigCard(pair: pair),
+            SizedBox(height: 10),
+            ElevatedButton(
+              onPressed: () => {
+                appState.getNext()
+              },
+              child: Text('next'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class BigCard extends StatelessWidget {
+  const BigCard({
+    super.key,
+    required this.pair,
+  });
+
+  final WordPair pair;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final baseStyle = TextStyle(color: theme.colorScheme.onPrimary);
+    final style = baseStyle.copyWith(
+      fontSize: theme.textTheme.displayMedium!.fontSize,
+      fontWeight: theme.textTheme.displayMedium!.fontWeight,
+    );
+    // final style = theme.textTheme.displayMedium!.copyWith(
+    //   color: theme.colorScheme.onPrimary,
+    // );
+    return Card(
+      color: theme.colorScheme.primary,
+      child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
